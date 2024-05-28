804a2d9509a422fb6c8047de9442effc62451009
vic_pani26 <abad26.tareas@gmail.com>
2024-05-27 19:18:52 -0500

    Volumen agregado

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,7 +8,29 @@ public class MainMenu : MonoBehaviour
 
     public void EscenaColiseo()
     {
-        SceneManager.LoadScene("Coliseo");
+        SceneManager.LoadScene("Victor");
+    }
+
+    public void EscenaNiveles()
+    {
+        SceneManager.LoadScene("Niveles");
+    }
+
+    public void EscenaNvl1()
+    {
+        SceneManager.LoadScene("Nvl1");
+    }
+    public void EscenaNvl2()
+    {
+        SceneManager.LoadScene("Nvl2");
+    }
+    public void EscenaNvl3()
+    {
+        SceneManager.LoadScene("Nvl3");
+    }
+    public void EscenaNvl4()
+    {
+        SceneManager.LoadScene("Nvl4");
     }
 
     public void Salir()
